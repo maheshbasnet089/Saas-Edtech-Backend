20d42f61be29a091bc7a14d402a6dbaf81fde03d
maheshbasnet089 <basnetmanish089@gmail.com>
2025-05-30 10:38:57 +0545

    add db connection

--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ import app from "./src/app";
 import { config } from "dotenv";
 config()
 
+//database connection import 
+// import garena vane connect hudainw or file execute hudainw 
+import "./src/database/connection"
 function startServer(){
     const port = process.env.PORT 
     app.listen(port,function(){
