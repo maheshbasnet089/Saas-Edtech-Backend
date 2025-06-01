324b8d97c15f9d91167e17bbc0edbf6e2d8274e6
maheshbasnet089 <basnetmanish089@gmail.com>
2025-06-01 20:40:16 +0545

    encryption,decryption

---
+++ src/controller/globals/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+
+
+exports.registerUser = ()=>{
+
+}
+
+exports.loginUser = ()=>{
+
+}
+
+class AuthController{
+    static async registerUser(){
+
+    }
+    static async loginUser(){
+
+    }
+}
+
+
+module.exports = AuthController
