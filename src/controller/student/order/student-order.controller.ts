dc541a4229b88cd36ea5484a8527fa1c29110620
maheshbasnet089 <basnetmanish089@gmail.com>
2025-08-26 21:01:41 +0545

    esewa complete

--- src/controller/student/order/student-order.controller.ts
+++ src/controller/student/order/student-order.controller.ts
@@ -7,6 +7,7 @@ import { getSequelizeTypeByDesignType } from "sequelize-typescript";
 import { KhaltiPayment } from "./paymentIntegration";
 import axios from "axios";
 import generateSha256Hash from "../../../services/generateSha256Hash";
+import base64 from 'base-64'
 
 // upload.fields([{ name: 'avatar1', maxCount: 1 }, {name:'avatar2', maxCount:1},{name:'avatar3', maxCount : 1}]
 
@@ -89,6 +90,7 @@ const createStudentCourseOrder = async(req:IExtendedRequest,res:Response)=>{
          id VARCHAR(36) PRIMARY KEY DEFAULT (UUID()), 
             paymentMethod ENUM('esewa','khalti','cod'), 
             pidx VARCHAR(26),
+            transaction_uuid VARCHAR(150),
             paymentStatus ENUM('paid','pending','unpaid') DEFAULT('unpaid'),
             totalAmount VARCHAR(10) NOT NULL,
             orderId VARCHAR(100), 
@@ -124,29 +126,60 @@ const createStudentCourseOrder = async(req:IExtendedRequest,res:Response)=>{
                  tax_amount : 0,
                  product_service_charge : 0,
                  product_delivery_charge : 0 ,
-                 product_code : process.env.ESEWA_PRODUCT_CODE ,
-                 totalAmount : amount,
-                 transaction_uuid : orderDetailsData[0].courseId,
+                 product_code : process.env.ESEWA_PRODUCT_CODE,
+                 amount : amount,
+                 total_amount : amount,
+                 transaction_uuid : userId + "_" + orderDetailsData[0].courseId,
                  success_url : "http://localhost:3000/", 
                  failure_url  : "http://localhost:3000/failure", 
                  signed_field_names : "total_amount,transaction_uuid,product_code"
             }
-            const data = `total_amount=${paymentData.totalAmount},transaction_uuid=${paymentData.transaction_uuid},product_code=${paymentData.product_code}`
+
+//             const paymentData = {
+// "amount": "100",
+// "failure_url": "https://developer.esewa.com.np/failure",
+// "product_delivery_charge": "0",
+// "product_service_charge": "0",
+// "product_code": "EPAYTEST",
+// // "signature": "i94zsd3oXF6ZsSr/kGqT4sSzYQzjj1W/waxjWyRwaME=",
+// "signed_field_names": "total_amount,transaction_uuid,product_code",
+// "success_url": "https://developer.esewa.com.np/success",
+// "tax_amount": "10",
+// "total_amount": "110",
+// "transaction_uuid": "2418285"
+// }
+ 
+            const data = `total_amount=${paymentData.total_amount},transaction_uuid=${paymentData.transaction_uuid},product_code=${paymentData.product_code}`
+
+            console.log(data,"This is data")
             const esewaSecretKey = process.env.ESEWA_SECRET_KEY
             const signature = generateSha256Hash(data,esewaSecretKey as string)
-
+            console.log(signature, esewaSecretKey,"This is signature")
+            console.log(paymentData,"PaymentData")
            const response = await axios.post("https://rc-epay.esewa.com.np/api/epay/main/v2/form",{
-                ...paymentData, signature
-            },{
+            ...paymentData, signature
+           },{
                 headers : {
                     "Content-Type" : "application/x-www-form-urlencoded"
                 }
             })
 
-            console.log(response)
+            console.log(response.request.res.responseUrl,"This is response")
+            if(response.status === 200){
+                    await sequelize.query(`INSERT INTO student_payment_${userId}(paymentMethod,totalAmount,orderId,transaction_uuid) VALUES(?,?,?,?)`,{
+            type : QueryTypes.INSERT, 
+            replacements : [paymentMethod,amount,result.id,paymentData.transaction_uuid]
+        })
+
+                res.status(200).json({
+                    message : "Payment initiated", 
+                    data : response.request.res.responseUrl
+                })
+            }
 
             // esewa integration function call here 
         }else if(paymentMethod === PaymentMethod.KHALTI){
+
             // khalti integration function call here 
          const response = await KhaltiPayment({
             amount : amount, 
@@ -157,6 +190,11 @@ const createStudentCourseOrder = async(req:IExtendedRequest,res:Response)=>{
          })
          if(response.status === 200){
             pidx = response.data.pidx
+                await sequelize.query(`INSERT INTO student_payment_${userId}(paymentMethod,totalAmount,orderId,pidx) VALUES(?,?,?,?)`,{
+            type : QueryTypes.INSERT, 
+            replacements : [paymentMethod,amount,result.id,pidx]
+        })
+
              res.status(200).json({
                  message : "Takethis", 
                  data : response.data
@@ -173,10 +211,7 @@ const createStudentCourseOrder = async(req:IExtendedRequest,res:Response)=>{
         }else{
             // stripe 
         }
-    await sequelize.query(`INSERT INTO student_payment_${userId}(paymentMethod,totalAmount,orderId,pidx) VALUES(?,?,?,?)`,{
-            type : QueryTypes.INSERT, 
-            replacements : [paymentMethod,amount,result.id,pidx]
-        })
+  
 
 }
 
@@ -206,4 +241,39 @@ const studentCoursePaymentVerification = async(req:IExtendedRequest,res:Response
     }
 }
 
-export {createStudentCourseOrder,studentCoursePaymentVerification}
+
+const studentCourseEsewaPaymentVerification = async(req:IExtendedRequest,res:Response)=>{
+    const {encodedData} = req.body 
+    const userId = req.user?.id
+    if(!encodedData) return res.status(400).json({
+        message : "Pleasr provide data base64 for verification"
+    })
+
+    
+    const result = base64.decode(encodedData)
+        const newresult:{
+            total_amount : string, 
+            transaction_uuid : string
+        } = JSON.parse(result)
+
+
+    const response = await axios.get(`https://rc.esewa.com.np/api/epay/transaction/status/?product_code=EPAYTEST&total_amount=${newresult.total_amount}&transaction_uuid=${newresult.transaction_uuid}`)
+    // console.log(response.data)
+    if(response.status === 200 && response.data.status === "COMPLETE"){
+  await sequelize.query(`UPDATE student_payment_${userId} SET paymentStatus=? WHERE transaction_uuid=?`,{
+            type : QueryTypes.UPDATE, 
+            replacements : ['paid',newresult.transaction_uuid]
+        })
+        res.status(200).json({
+            message : "Payment verified successfully"
+        })
+    }else{
+        res.status(500).json({
+            message : "Not verified"
+        })
+    }
+   
+
+}
+
+export {createStudentCourseOrder,studentCoursePaymentVerification,studentCourseEsewaPaymentVerification}
--- src/route/student/order/student-order.route.ts
+++ src/route/student/order/student-order.route.ts
@@ -9,14 +9,15 @@ import express,{Router} from 'express'
 import asyncErrorHandler from '../../../services/asyncErrorHandler';
 import { changeUserIdForTableName, isLoggedIn, restrictTo } from '../../../middleware/middleware';
 import { UserRole } from '../../../middleware/type';
-import { createStudentCourseOrder, studentCoursePaymentVerification } from '../../../controller/student/order/student-order.controller';
+import { createStudentCourseOrder, studentCourseEsewaPaymentVerification, studentCoursePaymentVerification } from '../../../controller/student/order/student-order.controller';
 
 
 const router:Router = express.Router()
 
 router.route("/order").post(isLoggedIn, changeUserIdForTableName, restrictTo(UserRole.Student), asyncErrorHandler(createStudentCourseOrder))
 
-router.route("/order/verify-transaction").post(isLoggedIn,changeUserIdForTableName,restrictTo(UserRole.Student),asyncErrorHandler(studentCoursePaymentVerification))
+router.route("/order/khalti/verify-transaction").post(isLoggedIn,changeUserIdForTableName,restrictTo(UserRole.Student),asyncErrorHandler(studentCoursePaymentVerification))
+router.route("/order/esewa/verify-transaction").post(isLoggedIn,changeUserIdForTableName,restrictTo(UserRole.Student),asyncErrorHandler(studentCourseEsewaPaymentVerification))
 
 
 export default router;
